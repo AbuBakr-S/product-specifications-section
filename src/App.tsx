696137bfa22cc024bfcb2d74cf960338fe6521be
AbuBakr-S <abz.siddique@gmail.com>
2025-07-28 16:45:33 +0100

    feat: add main title and text body

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
 
   return (
-    <h1>Product Specifications Section Home</h1>
+    <main className="p-4">
+      <section className="mt-12 mx-4">
+        <header>
+          <h1 className="text-3xl">Discover timeless elegance</h1>
+        </header>
+        <p className="text-lg mt-6 text-secondary font-sans font-normal">
+          Step into a world where quality meets quintessential charm with our collection. Every thread weaves a promise of unparalleled quality, ensuring that each garment is not just a part of your wardrobe, but a piece of art. Here's the essence of what makes our apparel the hallmark for those with an eye for excellence and a heart for the environment.
+        </p>
+      </section>
+    </main>
   )
 }
 
